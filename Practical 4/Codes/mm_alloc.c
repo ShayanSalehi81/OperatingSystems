4a97ab9d854466ca740135357dba5ad4f009ce99
Eye Patch <s.salehi1381@gmail.com>
2024-02-05 17:44:49 +0330

    Changed mm_alloc.c

--- Practical 4/Codes/mm_alloc.c
+++ Practical 4/Codes/mm_alloc.c
@@ -1,10 +1,65 @@
 #include "mm_alloc.h"
 #include <unistd.h>
 #include <stdlib.h>
+#include <string.h> 
 
 /* Your final implementation should comment out this macro. */
 
 s_block_ptr base = NULL;
+
+s_block_ptr find_block(s_block_ptr *last, size_t size) {
+    s_block_ptr b = base;
+    while (b && !(b->free && b->size >= size)) {
+        *last = b;
+        b = b->next;
+    }
+    return b;
+}
+
+void split_block(s_block_ptr b, size_t size) {
+    s_block_ptr newb;
+    newb = (s_block_ptr)(b->data + size);
+    newb->size = b->size - size - BLOCK_SIZE;
+    newb->next = b->next;
+    newb->free = 1;
+    newb->ptr = newb->data;
+    b->size = size;
+    b->next = newb;
+    if (newb->next)
+        newb->next->prev = newb;
+}
+
+s_block_ptr fusion(s_block_ptr b) {
+    if (b->next && b->next->free) {
+        b->size += BLOCK_SIZE + b->next->size;
+        b->next = b->next->next;
+        if (b->next)
+            b->next->prev = b;
+    }
+    return b;
+}
+
+s_block_ptr get_block(void *p) {
+    char *tmp;
+    tmp = p;
+    return (p = tmp -= BLOCK_SIZE);
+}
+
+
+s_block_ptr extend_heap(s_block_ptr last, size_t size) {
+    s_block_ptr b;
+    b = (s_block_ptr)sbrk(0);
+    if (sbrk(BLOCK_SIZE + size) == (void*)-1)
+        return NULL;
+    b->size = size;
+    b->next = NULL;
+    b->prev = last;
+    b->ptr = b->data;
+    if (last)
+        last->next = b;
+    b->free = 0;
+    return b;
+}
 
 int valid_addr(void *p) {
     if (base) {
--- Practical 4/Codes/mm_alloc.h
+++ Practical 4/Codes/mm_alloc.h
@@ -16,15 +16,12 @@
 extern "C" {
 #endif
 
+#include <unistd.h>
 #include <stdlib.h>
-
-void* mm_malloc(size_t size);
-void* mm_realloc(void* ptr, size_t size);
-void mm_free(void* ptr);
+#include <string.h> 
 
 typedef struct s_block *s_block_ptr;
 
-/* block struct */
 struct s_block {
     size_t size;
     struct s_block *next;
@@ -35,64 +32,17 @@ struct s_block {
     char data [0];
  };
 
-s_block_ptr find_block(s_block_ptr *last, size_t size) {
-    s_block_ptr b = base;
-    while (b && !(b->free && b->size >= size)) {
-        *last = b;
-        b = b->next;
-    }
-    return b;
-}
+void* mm_malloc(size_t size);
+void* mm_realloc(void* ptr, size_t size);
+void mm_free(void* ptr);
 
-/* Split block according to size, b must exist */
-void split_block(s_block_ptr b, size_t size) {
-    s_block_ptr newb;
-    newb = (s_block_ptr)(b->data + size);
-    newb->size = b->size - size - BLOCK_SIZE;
-    newb->next = b->next;
-    newb->free = 1;
-    newb->ptr = newb->data;
-    b->size = size;
-    b->next = newb;
-    if (newb->next)
-        newb->next->prev = newb;
-}
+extern s_block_ptr base;
 
-/* Try fusing block with neighbors */
-s_block_ptr fusion(s_block_ptr b) {
-    if (b->next && b->next->free) {
-        b->size += BLOCK_SIZE + b->next->size;
-        b->next = b->next->next;
-        if (b->next)
-            b->next->prev = b;
-    }
-    return b;
-}
-
-/* Get the block from addr */
-s_block_ptr get_block(void *p) {
-    char *tmp;
-    tmp = p;
-    return (p = tmp -= BLOCK_SIZE);
-}
-
-/* Add a new block at the of heap,
- * return NULL if things go wrong
- */
-s_block_ptr extend_heap(s_block_ptr last, size_t size) {
-    s_block_ptr b;
-    b = (s_block_ptr)sbrk(0);
-    if (sbrk(BLOCK_SIZE + size) == (void*)-1)
-        return NULL;
-    b->size = size;
-    b->next = NULL;
-    b->prev = last;
-    b->ptr = b->data;
-    if (last)
-        last->next = b;
-    b->free = 0;
-    return b;
-}
+s_block_ptr find_block(s_block_ptr *last, size_t size);
+void split_block(s_block_ptr b, size_t size);
+s_block_ptr fusion(s_block_ptr b);
+s_block_ptr get_block(void *p);
+s_block_ptr extend_heap(s_block_ptr last, size_t size);
 
 
 #ifdef __cplusplus
