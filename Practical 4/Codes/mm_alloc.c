7658b3e0c97555fafd77c0e2c58850a03cbe435e
Eye Patch <s.salehi1381@gmail.com>
2024-02-05 14:31:31 +0330

    Completing Practical 4 Codes

--- Practical 4/Codes/mm_alloc.c
+++ Practical 4/Codes/mm_alloc.c
@@ -1,40 +1,92 @@
-/*
- * mm_alloc.c
- *
- * Stub implementations of the mm_* routines. Remove this comment and provide
- * a summary of your allocator's design here.
- */
-
 #include "mm_alloc.h"
-
+#include <unistd.h>
 #include <stdlib.h>
 
 /* Your final implementation should comment out this macro. */
-#define MM_USE_STUBS
 
-void* mm_malloc(size_t size)
-{
-#ifdef MM_USE_STUBS
-    return calloc(1, size);
-#else
-#error Not implemented.
-#endif
+s_block_ptr base = NULL;
+
+int valid_addr(void *p) {
+    if (base) {
+        if (p > base && p < sbrk(0)) {
+            return p == (get_block(p))->ptr;
+        }
+    }
+    return 0;
 }
 
-void* mm_realloc(void* ptr, size_t size)
-{
-#ifdef MM_USE_STUBS
-    return realloc(ptr, size);
-#else
-#error Not implemented.
-#endif
+void* mm_malloc(size_t size) {
+    s_block_ptr b, last;
+    size_t s;
+    s = size;
+    if (base) {
+        last = base;
+        b = find_block(&last, s);
+        if (b) {
+            if ((b->size - s) >= (BLOCK_SIZE + 4))
+                split_block(b, s);
+            b->free = 0;
+        } else {
+            b = extend_heap(last, s);
+            if (!b)
+                return NULL;
+        }
+    } else {
+        b = extend_heap(NULL, s);
+        if (!b)
+            return NULL;
+        base = b;
+    }
+    return b->data;
 }
 
-void mm_free(void* ptr)
-{
-#ifdef MM_USE_STUBS
-    free(ptr);
-#else
-#error Not implemented.
-#endif
+void* mm_realloc(void* ptr, size_t size) {
+    size_t s;
+    s_block_ptr b, newb;
+    void *newp;
+    if (!ptr)
+        return mm_malloc(size);
+    if (valid_addr(ptr)) {
+        s = size;
+        b = get_block(ptr);
+        if (b->size >= s) {
+            if (b->size - s >= (BLOCK_SIZE + 4))
+                split_block(b, s);
+        } else {
+            if (b->next && b->next->free && (b->size + BLOCK_SIZE + b->next->size) >= s) {
+                fusion(b);
+                if (b->size - s >= (BLOCK_SIZE + 4))
+                    split_block(b, s);
+            } else {
+                newp = mm_malloc(s);
+                if (!newp)
+                    return NULL;
+                newb = get_block(newp);
+                memcpy(newb->ptr, b->ptr, b->size);
+                mm_free(ptr);
+                return newp;
+            }
+        }
+        return ptr;
+    }
+    return NULL;
+}
+
+void mm_free(void* ptr) {
+    s_block_ptr b;
+    if (valid_addr(ptr)) {
+        b = get_block(ptr);
+        b->free = 1;
+        if (b->prev && b->prev->free)
+            b = fusion(b->prev);
+        if (b->next)
+            fusion(b);
+        else {
+            if (b->prev)
+                b->prev->next = NULL;
+            else
+                base = NULL;
+            brk(b);
+        }
+    }
 }
--- Practical 4/Codes/mm_alloc.h
+++ Practical 4/Codes/mm_alloc.h
@@ -22,7 +22,6 @@ void* mm_malloc(size_t size);
 void* mm_realloc(void* ptr, size_t size);
 void mm_free(void* ptr);
 
-
 typedef struct s_block *s_block_ptr;
 
 /* block struct */
@@ -36,19 +35,64 @@ struct s_block {
     char data [0];
  };
 
+s_block_ptr find_block(s_block_ptr *last, size_t size) {
+    s_block_ptr b = base;
+    while (b && !(b->free && b->size >= size)) {
+        *last = b;
+        b = b->next;
+    }
+    return b;
+}
+
 /* Split block according to size, b must exist */
-void split_block (s_block_ptr b, size_t s);
+void split_block(s_block_ptr b, size_t size) {
+    s_block_ptr newb;
+    newb = (s_block_ptr)(b->data + size);
+    newb->size = b->size - size - BLOCK_SIZE;
+    newb->next = b->next;
+    newb->free = 1;
+    newb->ptr = newb->data;
+    b->size = size;
+    b->next = newb;
+    if (newb->next)
+        newb->next->prev = newb;
+}
 
 /* Try fusing block with neighbors */
-s_block_ptr fusion(s_block_ptr b);
+s_block_ptr fusion(s_block_ptr b) {
+    if (b->next && b->next->free) {
+        b->size += BLOCK_SIZE + b->next->size;
+        b->next = b->next->next;
+        if (b->next)
+            b->next->prev = b;
+    }
+    return b;
+}
 
 /* Get the block from addr */
-s_block_ptr get_block (void *p);
+s_block_ptr get_block(void *p) {
+    char *tmp;
+    tmp = p;
+    return (p = tmp -= BLOCK_SIZE);
+}
 
 /* Add a new block at the of heap,
  * return NULL if things go wrong
  */
-s_block_ptr extend_heap (s_block_ptr last , size_t s);
+s_block_ptr extend_heap(s_block_ptr last, size_t size) {
+    s_block_ptr b;
+    b = (s_block_ptr)sbrk(0);
+    if (sbrk(BLOCK_SIZE + size) == (void*)-1)
+        return NULL;
+    b->size = size;
+    b->next = NULL;
+    b->prev = last;
+    b->ptr = b->data;
+    if (last)
+        last->next = b;
+    b->free = 0;
+    return b;
+}
 
 
 #ifdef __cplusplus
